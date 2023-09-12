99a211519f905f613cf526e4a915fdfe2a43b1bb
Sue216 <sunerah16@gmail.com>
2023-09-12 07:48:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints _putchar
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
